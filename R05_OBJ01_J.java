d2981cde80d09643104efa795e39b592f39f1be0
AustinStafford12 <123527209+AustinStafford12@users.noreply.github.com>
2024-10-04 15:08:13 -0400

    Create R05_OBJ01_J.java

---
+++ R05_OBJ01_J.java
@@ -0,0 +1,22 @@
+// Rule 05. Object Orientation (OBJ)
+public class Widget {
+  public int total; // Number of elements
+ 
+  void add() {
+    if (total < Integer.MAX_VALUE) {     
+      total++;
+      // ...
+    } else {
+      throw new ArithmeticException("Overflow");
+    }
+  }
+ 
+  void remove() { 
+    if (total > 0) {     
+      total--;
+      // ...
+    } else {
+      throw new ArithmeticException("Overflow");
+    }
+  }
+}
