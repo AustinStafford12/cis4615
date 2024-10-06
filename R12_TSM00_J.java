001082215f77b1b15759cda2c2c07c918b0981eb
AustinStafford12 <123527209+AustinStafford12@users.noreply.github.com>
2024-10-06 13:51:42 -0400

    Create R12_TSM00_J.java

---
+++ R12_TSM00_J.java
@@ -0,0 +1,12 @@
+// Rule 12. Thread-Safety Miscellaneous (TSM)
+class Base {
+  public synchronized void doSomething() {
+    // ...
+  }
+}
+ 
+class Derived extends Base {
+  @Override public void doSomething() {
+    // ...
+  }
+}
