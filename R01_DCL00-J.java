51f7ccabbe3d58b30819966b204808321e0abbbe
AustinStafford12 <123527209+AustinStafford12@users.noreply.github.com>
2024-10-06 14:02:29 -0400

    Update R01_DCL00-J.java

--- R01_DCL00-J.java
+++ R01_DCL00-J.java
@@ -1,9 +1,9 @@
 // Rule 01. Declarations and Initialization (DCL)
+// compliant code
 public class Cycle {
   private final int balance;
-  private static final Cycle c = new Cycle();
   private static final int deposit = (int) (Math.random() * 100); // Random deposit
- 
+  private static final Cycle c = new Cycle();
   public Cycle() {
     balance = deposit - 10; // Subtract processing fee
   }
