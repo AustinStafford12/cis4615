679983e7846c60de897042e7131d31f6a0d62032
AustinStafford12 <123527209+AustinStafford12@users.noreply.github.com>
2024-10-06 14:09:57 -0400

    Update R05_OBJ01_J.java

--- R05_OBJ01_J.java
+++ R05_OBJ01_J.java
@@ -1,7 +1,14 @@
 // Rule 05. Object Orientation (OBJ)
+// compliant solution
 public class Widget {
-  public int total; // Number of elements
+  private int total; // Declared private
  
+  public int getTotal () {
+    return total;
+  }
+ 
+  // Definitions for add() and remove() remain the same
+
   void add() {
     if (total < Integer.MAX_VALUE) {     
       total++;
